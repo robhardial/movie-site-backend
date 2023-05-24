e3d39c4885ed4c75c6aa8e6e57030fb2b591ed45
Rob <roberthardial@gmail.com>
2023-05-24 00:07:02 -0400

    Your commit message

---
+++ src/main/java/dev/rob/movies/MovieRepository.java
@@ -0,0 +1,10 @@
+package dev.rob.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    <Optional>Movie findMovieByImdbId(String imdbId);
+}
